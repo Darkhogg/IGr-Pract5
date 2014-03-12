001e54a77f876b98b8e602ae58fd5f690e05e501
Daniel Escoz <darkhogg@gmail.com>
2014-03-12 18:06:16 +0100

    Added motion and better axis rendering

--- extrude_scene.cpp
+++ extrude_scene.cpp
@@ -12,6 +12,20 @@ void igr::extrude_scene::on_begin () {
   gluPerspective(45, (float) window.getSize().x / (float) window.getSize().y, 1, 1000);
 }
 
+
+void igr::extrude_scene::on_update (float delta) {
+  _vyaw   += _vyawspd   * delta;
+  _vfar   += _vfarspd   * delta;
+  _vpitch += _vpitchspd * delta;
+
+  cam.eye = {
+    _vfar * cos(_vyaw) * cos(_vpitch),
+    _vfar * sin(_vpitch),
+    _vfar * sin(_vyaw) * cos(_vpitch)
+  };
+}
+
+
 void igr::extrude_scene::on_draw () {
   /* Clear the screen */
   glClearColor(0.f, 0.f, 0.f, 1.f);
@@ -20,18 +34,32 @@ void igr::extrude_scene::on_draw () {
   /* Draw axis to check everything ok */
   glBegin(GL_LINES);
     glColor3f(1.f, 0.f, 0.f);
-    glVertex3f(-100.f, 0.f, 0.f);
-    glVertex3f(+100.f, 0.f, 0.f);
+    glVertex3f(-10.f, 0.f, 0.f);
+    glVertex3f(+10.f, 0.f, 0.f);
+    glVertex3f(10.f, +0.2f, 0.f);
+    glVertex3f(10.f, -0.2f, 0.f);
+    glVertex3f(10.f, 0.f, +0.2f);
+    glVertex3f(10.f, 0.f, -0.2f);
 
-    glColor3f(0.f, 1.f, 0.f);
-    glVertex3f(0.f, -100.f, 0.f);
-    glVertex3f(0.f, +100.f, 0.f);
 
-    glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(0.f, 0.f, -100.f);
-    glVertex3f(0.f, 0.f, +100.f);
+    glColor3f(0.f, 0.8f, 0.f);
+    glVertex3f(0.f, -10.f, 0.f);
+    glVertex3f(0.f, +10.f, 0.f);
+    glVertex3f(+0.2f, 10.f, 0.f);
+    glVertex3f(-0.2f, 10.f, 0.f);
+    glVertex3f(0.f, 10.f, +0.2f);
+    glVertex3f(0.f, 10.f, -0.2f);
+
+    glColor3f(0.f, 0.5f, 1.f);
+    glVertex3f(0.f, 0.f, -10.f);
+    glVertex3f(0.f, 0.f, +10.f);
+    glVertex3f(+0.2f, 0.f, 10.f);
+    glVertex3f(-0.2f, 0.f, 10.f);
+    glVertex3f(0.f, +0.2f, 10.f);
+    glVertex3f(0.f, -0.2f, 10.f);
   glEnd();
 }
+
 
 bool igr::extrude_scene::on_event (event_t event) {
   switch (event.type) {
@@ -45,10 +73,68 @@ bool igr::extrude_scene::on_event (event_t event) {
       return true;
     }
 
+    case sf::Event::KeyPressed: {
+      switch (event.key.code) {
+        case sf::Keyboard::Key::A: {
+          _vyawspd = 1.0;
+          break;
+        }
+        case sf::Keyboard::Key::D: {
+          _vyawspd = -1.0;
+          break;
+        }
+
+        case sf::Keyboard::Key::W: {
+          _vfarspd = -16.0;
+          break;
+        }
+        case sf::Keyboard::Key::S: {
+          _vfarspd = 16.0;
+          break;
+        }
+
+        case sf::Keyboard::Key::Q: {
+          _vpitchspd = 1.0;
+          break;
+        }
+        case sf::Keyboard::Key::E: {
+          _vpitchspd = -1.0;
+          break;
+        }
+
+        default: {}
+      }
+
+      return true;
+    }
+
+    case sf::Event::KeyReleased: {
+      switch (event.key.code) {
+        case sf::Keyboard::Key::A:
+        case sf::Keyboard::Key::D: {
+          _vyawspd = 0.0;
+          break;
+        }
+        case sf::Keyboard::Key::W:
+        case sf::Keyboard::Key::S: {
+          _vfarspd = 0.0;
+          break;
+        }
+        case sf::Keyboard::Key::Q:
+        case sf::Keyboard::Key::E: {
+          _vpitchspd = 0.0;
+          break;
+        }
+
+        default: {}
+      }
+    }
+
     default:
       return false;
   }
 }
+
 
 /* Definition of main here */
 int main () {
--- extrude_scene.hpp
+++ extrude_scene.hpp
@@ -7,10 +7,18 @@
 namespace igr {
 
   class extrude_scene : public scene<extrude_scene> {
+      double _vyaw,   _vyawspd;
+      double _vpitch, _vpitchspd;
+      double _vfar,   _vfarspd;
 
     public:
+      extrude_scene ()
+        : _vyaw(1.0), _vpitch(0.5), _vfar(16.0)
+      {}
+
       void on_begin ();
       void on_draw ();
+      void on_update (float delta);
       bool on_event (event_t event);
   };
 
